3094b32c0351a68d3bb6215ebdbc711b677a40b3
rupak-sarkar <rupak.sar@gmail.com>
2025-11-01 12:29:06 +0530

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -8,21 +8,18 @@ end_date = datetime.now().date()
 start_date = end_date - timedelta(days=2)
 output_file = 'stock_data_last_2_days.csv'
 
-all_data = []
+final_df=pd.DataFrame()
 for ticker in tickers:
     df = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
     if not df.empty:
         df = df.reset_index()
+        df.columns=df.columns.get_level_values("Price")
         df['Ticker'] = ticker
         df = df[['Date', 'Ticker', 'Open', 'High', 'Low', 'Close', 'Volume']]
-        all_data.append(df)
-
-if all_data:
-    final_df = pd.concat(all_data, ignore_index=True)
-    final_df.rename(columns={'Date': 'date'}, inplace=True)
-    final_df['date'] = pd.to_datetime(final_df['date']).dt.strftime('%m/%d/%Y')
-    final_df = final_df.round(2)
+        final_df=pd.concat([final_df,df])
+if final_df:
     final_df.to_csv(output_file, index=False)
     print(f"✅ Saved {len(final_df)} rows to {output_file}")
 else:
     print("⚠️ No data fetched (check network or ticker symbols)")
+
