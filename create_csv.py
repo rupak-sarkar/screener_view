742266d818fa111c31bcace128f131aaefbc09ec
rupak-sarkar <rupak.sar@gmail.com>
2025-11-07 15:39:15 +0530

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -30,26 +31,21 @@ df_scraped['Company Name'] = df_scraped['Column1'].str.split('/').str[2]
 df_scraped['Final Ticker'] = df_scraped['Company Name'] + '.NS'
 df_scraped = df_scraped.drop_duplicates(subset='Final Ticker')
 
-# Step 2: Fetch stock data using yfinance
 tickers = df_scraped['Final Ticker'].tolist()
 end_date = datetime.now().date()
-start_date = end_date - timedelta(days=365)
+start_date = end_date - timedelta(days=1)
 output_file = 'stock_data_last_2_days.csv'
 
-final_df = pd.DataFrame()
+final_df=pd.DataFrame()
 for ticker in tickers:
-    try:
-        df = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
-        if not df.empty:
-            df = df.reset_index()
-            df['Ticker'] = ticker
-            df = df[['Date', 'Ticker', 'Open', 'High', 'Low', 'Close', 'Volume']]
-            df[['Open', 'High', 'Low', 'Close']] = df[['Open', 'High', 'Low', 'Close']].round(2)
-            final_df = pd.concat([final_df, df])
-    except Exception as e:
-        print(f"Error fetching data for {ticker}: {e}")
-
-# Step 3: Save to CSV
+    df = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
+    if not df.empty:
+        df = df.reset_index()
+        df.columns=df.columns.get_level_values("Price")
+        df['Ticker'] = ticker
+        df = df[['Date', 'Ticker', 'Open', 'High', 'Low', 'Close', 'Volume']]
+        df[['Open', 'High', 'Low', 'Close']] = df[['Open', 'High', 'Low', 'Close']].round(2)
+        final_df=pd.concat([final_df,df])
 if not final_df.empty:
     final_df.to_csv(output_file, mode='a', header=not os.path.exists(output_file), index=False)
     print(f"✅ Saved {len(final_df)} rows to {output_file}")
